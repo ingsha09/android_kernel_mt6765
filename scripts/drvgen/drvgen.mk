d06dc03c458a73de1752774f0bd02038e372199f
Ingsha Hang Subba <ingshalimbu09@gmail.com>
2026-09-09 12:04:01 +0530

    Add temporary file creation for dtboimg and dtbimg

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -87,6 +87,7 @@ dtbs: $(objtree)/dtboimg.cfg $(objtree)/dtbimg.cfg
 
 $(objtree)/dtboimg.cfg: FORCE
 	rm -f $@.tmp
+	touch $@.tmp
 	$(foreach f,$(ABS_DTB_FILES),$(call mk_dtboimg_cfg,$(f),$@.tmp))
 	if ! cmp -s $@.tmp $@; then \
 		mv $@.tmp $@; \
@@ -96,6 +97,7 @@ $(objtree)/dtboimg.cfg: FORCE
 
 $(objtree)/dtbimg.cfg: FORCE
 	rm -f $@.tmp
+	touch $@.tmp
 	$(foreach f,$(ABS_DTB2_FILES),$(call mk_dtbimg_cfg,$(f),$@.tmp))
 	if ! cmp -s $@.tmp $@; then \
 		mv $@.tmp $@; \
